747a8fa804fd4dac33680d2b54b2f8a8326392c0
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-23 15:19:50 -0400

    update as there was amastake

--- play.js
+++ play.js
@@ -1,16 +1,11 @@
-// const connect = require("./client");
-// console.log("Connecting ...");
-// connect();
+const connect = require("./client");
+console.log("Connecting ...");
+connect();
 
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   return stdin;
-// };
-
-const jsonString = '{"a":1, "b":2, "foo":"bar"}';
-const obj = JSON.parse(jsonString);
-delete obj.foo;
-console.log(obj);
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
