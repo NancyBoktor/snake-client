da056e9933d31ed5197fe182bd1122ce70bfdb0d
Nancy Boktor <nancyboktor1@gmail.com>
2021-09-23 15:16:07 -0400

    Moving our Snake

--- client.js
+++ client.js
@@ -12,6 +12,34 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: NBS");
+    const handleUserInput = function (key) {
+      if (key === "\u0003") {
+        process.exit();
+      }
+      if (key === "w") {
+        conn.write("Move: up");
+      }
+      if (key === "s") {
+        conn.write("Move: down");
+      }
+      if (key === "a") {
+        conn.write("Move: left");
+      }
+      if (key === "d") {
+        conn.write("Move: right");
+      }
+    };
+
+    const setupInput = function () {
+      const stdin = process.stdin;
+      stdin.setRawMode(true);
+      stdin.setEncoding("utf8");
+      stdin.resume();
+      stdin.on("data", handleUserInput);
+      return stdin;
+    };
+
+    setupInput();
   });
 
   conn.on("error", () => {
